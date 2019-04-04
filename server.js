d720acd4c88509c89dbc12ca399c7cecb49e9609
venkatesh.m@brillio.com <venkatesh.m@brillio.com>
2019-04-04 13:02:39 +0530

    After removing previous edit form

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/update', function(req, res) {
                 }
             }
         );
-        consloe.log('test '+req.body.Markup.trim());
+        //consloe.log('test '+req.body.Markup.trim());
     });
 });
 
